2ed4ed7510c332109b703cc6d4a9b76476e2ef6f
seunghyo <tmdgy6232@naver.com>
2022-11-21 20:20:41 +0900

    first commit

---
+++ src/main/java/boot/startboot/StartBootApplication.java
@@ -0,0 +1,13 @@
+package boot.startboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boot/startboot/StartBootApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.startboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
