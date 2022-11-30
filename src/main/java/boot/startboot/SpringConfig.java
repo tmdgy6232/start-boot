35c80f7a25850e20b60ad1b1ef201313467e3604
seunghyo <tmdgy6232@naver.com>
2022-11-30 22:55:33 +0900

    Spring Jpa Example

--- src/main/java/boot/startboot/SpringConfig.java
+++ src/main/java/boot/startboot/SpringConfig.java
@@ -1,37 +1,31 @@
 package boot.startboot;
 
-import boot.startboot.repository.JdbcMemberRepository;
-import boot.startboot.repository.JdbcTemplateMemberRepository;
-import boot.startboot.repository.JpaMemberRepository;
 import boot.startboot.repository.MemberRepository;
 import boot.startboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
     @Bean
     public MemberRepository memberRepository(){
        // return new JdbcMemberRepository(dataSource);
        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+//        return new JpaMemberRepository(em);
     }
 }
